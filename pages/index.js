e520e9b35446068d934e8839809044a3aa237b1e
admire Excel <godwillokenyi@gmail.com>
2023-06-13 04:24:40 +0100

    add description

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function HomePage() {
     <>
       <Head>
         <title>Godswill Okenyi Portfolio</title>
-        <meta name="description" content="Excel Mern Stack Developer" />
+        <meta name="This is a portfolio website made by Godswill Okenyi, Full stack Developer" content="Excel Mern Stack Developer" />
       </Head>
       <Heading />
     </>
